baddf237cd3e69e563818fef060406284693bed7
Mehul <mehulprajapati2802@gmail.com>
2025-07-26 11:35:02 +0530

    Fix all

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import PlaceOrder from './pages/PlaceOrder';
 import Orders from './pages/Orders';
 import Navbar from './components/Navbar.jsx';
 import Footer from './components/Footer';
-import SearchBar from './components/SearchBar';
+import SearchBar from './components/Searchbar.jsx';
 import { ToastContainer, toast } from 'react-toastify';
 
 function App() {
