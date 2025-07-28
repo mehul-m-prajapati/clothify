411f315cacdd069619d28e0eda5c8ae4d2b7eed4
Mehul <mehulprajapati2802@gmail.com>
2025-07-28 19:11:38 +0530

    added cart item count

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -2,7 +2,8 @@ import { createContext, useEffect, useReducer, useState } from "react";
 import { products } from "../assets/assets";
 import PropTypes from 'prop-types';
 import { useLocation } from "react-router-dom";
-
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 export const ShopContext = createContext();
 
@@ -13,6 +14,7 @@ const ShopContextProvider = ({children}) => {
     const [showSearch, setShowSearch] = useState(false);
     const [search, setSearch] = useState('');
     const location = useLocation();
+    const [cartItems, setCartItems] = useState({});
 
     useEffect(() => {
         if (location.pathname.includes('collection') )
@@ -22,14 +24,56 @@ const ShopContextProvider = ({children}) => {
 
     }, [location]);
 
-    const addToCart = async () => {
+    const addToCart = async (itemId, size) => {
 
-    }
+        if (!size) {
+            toast.error('Please select a size');
+            return;
+        }
+
+        let cartData = structuredClone(cartItems);
+
+        /*
+        const cartItems = {
+            "item123": {
+                "M": 2,
+                "L": 1
+            },
+            "item456": {
+                "S": 1
+            }
+        };
+        */
+        if (cartData[itemId]) {
+            cartData[itemId][size] ? (cartData[itemId][size] += 1) : (cartData[itemId][size] = 1);
+        } else {
+            cartData[itemId] = {};
+            cartData[itemId][size] = 1;
+        }
+
+        setCartItems(cartData);
+    };
 
     const addOrder = () => {
     }
 
     const getCartCount = () => {
+        let totalCount = 0;
+
+        for (const itemId in cartItems) {
+            const sizes = cartItems[itemId];
+            for (const sizeType in sizes) {
+                try {
+                    let sizeCnt = sizes[sizeType];
+                    if (sizeCnt > 0)
+                        totalCount += sizeCnt;
+                } catch (error) {
+                    console.log("Err: getCartCount:", error);
+                }
+            }
+        }
+
+        return totalCount;
     }
 
     const updateQuantity = async() => {
@@ -41,12 +85,13 @@ const ShopContextProvider = ({children}) => {
 
     const contextValue = {
         products,
+        currency,
+        delivery_fee,
         search,
         setSearch,
         showSearch,
         setShowSearch,
-        currency,
-        delivery_fee,
+        cartItems,
         addToCart,
         addOrder,
         getCartAmount,
