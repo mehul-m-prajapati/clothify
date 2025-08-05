9f098f61f152bbdedc25e0a69f5c4ed55e54d07a
Mehul <mehulprajapati2802@gmail.com>
2025-08-05 15:12:57 +0530

    add all pages

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -3,32 +3,44 @@ import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
 import Login from './components/Login'
 
+import Add from './pages/Add'
+import List from './pages/List'
+import Orders from './pages/Orders'
+
 import { ToastContainer } from 'react-toastify';
 import {Route, Routes} from 'react-router-dom'
+import { useEffect, useState } from 'react';
 
 export const backendUrl = import.meta.env.VITE_BACKEND_URL;
 export const currency = '$';
 
 function App() {
 
+  const [token, setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : '');
+
+  useEffect(() => {
+    localStorage.setItem('token', token);
+  }, [token]);
+
   return (
     <div className='bg-gray-50 min-h-screen'>
         <ToastContainer />
-
-        <>
-            <Navbar />
-            <hr />
-            <div className='flex w-full'>
-                <Sidebar />
-                <div className='w-[70%] mx-auto ml-[max(5vw, 25px)] my-8  text-gray-600 text-base'>
-                    <Routes>
-                        <Route path='/add' />
-                        <Route path='/list' />
-                        <Route path='/orders' />
-                    </Routes>
+        {token === '' ? (<Login setToken={setToken} />) : (
+            <>
+                <Navbar />
+                <hr />
+                <div className='flex w-full'>
+                    <Sidebar />
+                    <div className='w-[70%] mx-auto ml-[max(5vw, 25px)] my-8  text-gray-600 text-base'>
+                        <Routes>
+                            <Route path='/add' element={<Add token={token} />} />
+                            <Route path='/list' element={<List token={token} />} />
+                            <Route path='/orders' element={<Orders token={token} />} />
+                        </Routes>
+                    </div>
                 </div>
-            </div>
-        </>
+            </>
+        )}
     </div>
   )
 }
